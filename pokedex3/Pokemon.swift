f32227be1209d10b4379ca978cb7b549cc9fc587
Pavlos Nicolaou <pauln1993@gmail.com>
2016-08-17 17:08:11 +0300

    just created pokemon class

---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,33 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Pavlos Nicolaou on 17/08/2016.
+//  Copyright © 2016 Pavlos Nicolaou. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+        
+    }
+    
+}
