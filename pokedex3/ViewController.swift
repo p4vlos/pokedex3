7991da66436a2fc323b3939955a9755efdfe2430
Pavlos Nicolaou <pauln1993@gmail.com>
2016-08-19 19:13:26 +0300

    added pokemon music and stop button as well

--- pokedex3/ViewController.swift
+++ pokedex3/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var collection: UICollectionView!
     
     var pokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +24,23 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         collection.delegate = self
         
         parsePokemonCSV()
+        initAudio()
+    }
+    
+    func initAudio() {
+        
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")!
+        
+        do {
+            
+            musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
     
     func parsePokemonCSV() {
@@ -88,6 +107,21 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
     }
 
+    @IBAction func musicBtnPressed(_ sender: AnyObject) {
+        
+        if musicPlayer.isPlaying {
+            
+            musicPlayer.pause()
+            //sender.alpha = 0.2
+            
+        } else {
+            
+            musicPlayer.play()
+            //sender.alpha = 1.0
+        
+        }
+        
+    }
 
 }
 
