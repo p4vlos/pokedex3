29a852a751517a6f736b2f703f9719edbafaa778
Pavlos Nicolaou <pauln1993@gmail.com>
2016-08-22 12:14:36 +0300

    created the UI for each pokemon and installed alamofire to connect to pokemon api

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'pokedex3' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for pokedex3
+  pod 'Alamofire',
+  :git => 'https://github.com/Alamofire/Alamofire.git',
+  :branch => 'swift3'
+
+end
--- pokedex3/PokemonDetailVC.swift
+++ pokedex3/PokemonDetailVC.swift
@@ -12,7 +12,20 @@ class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     
+    
+    @IBOutlet weak var mainImg: UIImageView!
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +34,9 @@ class PokemonDetailVC: UIViewController {
 
     }
 
+    @IBAction func backBtnPressed(_ sender: AnyObject) {
+        
+        dismiss(animated: true, completion: nil)
+    }
    
 }
